468ffe5c42531ef231106931049249c1a811c0b5
cazterk <zcephas2@gmail.com>
2022-04-04 16:37:20 +0200

    display fetched data

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // import fetch from "node-fetch";
 
 const title = document.querySelector("#title");
-const body = document.querySelector("#body");
+const container = document.querySelector("#container");
 
 const url = "http://localhost:3000/api/v1/articles";
 
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
 console.log("Hello World");
+
+async function getArticles() {
+  let response = await fetch(url);
+  let data = await response.json();
+
+  const cleanedData = Object.keys(data).map((key) => data[key]);
+  const articles = cleanedData[2];
+
+  let mappedArticles = articles.map((article, index) => {
+    return `<div class="article" key=${index}>
+    <h2>${article.title}</h2>
+    <p>${article.body}</p>
+    </div>`;
+  });
+
+  container.innerHTML = mappedArticles;
+}
+
+console.log(getArticles());
